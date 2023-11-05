88763a85268181b5e5564ae63b6649a7985e03b8
Alan Taranti <contato@alantaranti.me>
2023-11-05 00:35:31 -0400

    adicionado componente maleavel

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,7 +1,65 @@
 <template>
-  <main class="container">Hello World!</main>
+  <main class="container">
+    <malleable-component />
+    <br />
+    <hr />
+    <br />
+    <form @submit.prevent="requestChanges">
+      <label for="prompt">Prompt:</label>
+      <input v-model="prompt" type="text" placeholder="Faz tudo melhor" />
+      <button :aria-busy="status === 'pending'" type="submit">Request Changes</button>
+    </form>
+  </main>
 </template>
 
 <script lang="ts" setup>
 import '@picocss/pico';
+
+const prompt = ref('');
+const status = ref<'idle' | 'pending'>('idle');
+
+const malleables = reactive<Record<string, any>>({
+  template: `<div class='container'>Crie algo aqui</div>`,
+  script: {
+    data: {},
+    methods: {},
+    computed: {},
+    watch: {},
+  },
+});
+
+const MalleableComponent = defineComponent({
+  setup() {
+    return () =>
+      h({
+        data: () => malleables.script?.data,
+        methods: Object.fromEntries(
+          Object.entries(malleables.script?.methods ?? {}).map(([key, value]) => [key, new Function('return ' + value)()]),
+        ),
+        computed: Object.fromEntries(
+          Object.entries(malleables.script?.computed ?? {}).map(([key, value]) => [key, new Function('return ' + value)()]),
+        ),
+        watch: Object.fromEntries(
+          Object.entries(malleables.script?.watch ?? {}).map(([key, value]) => [key, new Function('return ' + value)()]),
+        ),
+        template: malleables.template,
+      });
+  },
+});
+
+async function requestChanges() {
+  status.value = 'pending';
+  const data = await $fetch('/api/completion', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ prompt: prompt.value, malleables: malleables }),
+  });
+
+  for (const key in data.malleables) {
+    malleables[key] = data.malleables[key];
+  }
+  status.value = 'idle';
+}
 </script>
