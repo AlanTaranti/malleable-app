e8cbd2d82768c882674ff6c9a16c937884b9e5c8
Alan Taranti <contato@alantaranti.me>
2023-11-04 21:25:39 -0400

    adicionado firebase

---
+++ frontend/plugins/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBnISfC537EPFvRlW-ctaw2oix8fxZSnFE',
+  authDomain: 'malleable-app.firebaseapp.com',
+  projectId: 'malleable-app',
+  storageBucket: 'malleable-app.appspot.com',
+  messagingSenderId: '1000468865762',
+  appId: '1:1000468865762:web:cefa104ffa88ef69d434bb',
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
